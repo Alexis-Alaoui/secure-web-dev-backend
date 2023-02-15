af5f37d623f91b1657502e8bf6ffa953a4e3ac1d
alexis <alexis.alaoui@edu.devinci.fr>
2023-02-15 11:25:16 +0100

    test

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/users", usersController);
 app.get("/", (req, res) => res.status(200).json({ message: "Hello World !" }));
 
 async function main() {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(MONGO_URI);
   console.log("Connected to Mongo Database");
   app.listen(port, () => {
     console.log(
